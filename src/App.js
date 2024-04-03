87dc021cf52bc0dfff105b79ee0c875826f895ce
Yashar Ghasempour <yghasempour1@gmail.com>
2024-04-03 17:54:08 -0400

    The updated version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import React from 'react';
-import { Routes, Route} from 'react-router-dom';
-import Home from './Modules/Home';
-import SignUp from './Modules/SignUp';
-import SignIn from './Modules/SignIn';
-import Dashboard from './Modules/Dashboard';
-import AddProperty from './Modules/AddProperty';
+import "./App.css"
+import Pages from "./components/pages/Pages"
 
-const App = () => {
-  return (
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/addProperty" element={<AddProperty/>} />
-      </Routes>
-  );
-};
+function App() {
+  return <Pages />
+}
 
-export default App;
+export default App
